14d532c757f2bfb93ab5219e07a9eb4dbcc2cf01
Roman Salov <roman.salov@renlife.com>
2023-02-25 18:56:46 +0400

    feat: deploy script, remove dist from main

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+cd dist
+
+# Деплой в gh-pages
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:niodium/niodium.github.io.git main:gh-pages
+
+cd -
