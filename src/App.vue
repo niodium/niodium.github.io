06b881d2d89742a48c4eecfd941fd3226114da27
Roman Salov <roman.salov@renlife.com>
2023-02-26 17:34:02 +0400

    feat: some tests

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar fixed-top bg-body-secondary">
+    <div class="container">
+      <a class="navbar-brand" href="#">rnd.fun</a>
+      <button class="btn btn-outline-warning" @click="getRandomVideo">random</button>
+    </div>
+  </nav>
+  <main class="container">
+    <VideoList :currentVideo="currentVideo"/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VideoList from "@/Components/VideoList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    VideoList
+  },
+  data() {
+    return {
+      currentVideo: {},
+      videos: [
+        {"src": "https://giant.gfycat.com/FrayedMisguidedBedlingtonterrier.mp4", "type": "video/mp4"},
+        {"src": "https://zippy.gfycat.com/AdorableSaneArabianwildcat.mp4", "type": "video/mp4"},
+        {"src": "https://giant.gfycat.com/FelineDependableGalapagostortoise.mp4", "type": "video/mp4"}
+      ]
+    }
+  },
+  methods: {
+    getRandomVideo() {
+      let nextVideo = this.videos[Math.floor(Math.random() * this.videos.length)]
+
+      while (this.currentVideo === nextVideo) {
+        nextVideo = this.videos[Math.floor(Math.random() * this.videos.length)]
+      }
+      this.currentVideo = nextVideo
+    }
+  },
+  mounted() {
+    this.getRandomVideo()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/Components/VideoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <video class="object-fit-contain" controls autoplay loop :key="currentVideo">
+      <source :src="currentVideo.src" :type="currentVideo.type">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoList",
+  props: {
+    currentVideo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
